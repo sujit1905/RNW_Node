5f65fdb327a25a7c13ebc3f89c9f585fbf14a74d
Sujit Mecwan <mecwansujit@gmail.com>
2026-04-18 10:52:11 +0530

    frontend api

--- task1/server.js
+++ task1/server.js
@@ -1,38 +1,39 @@
-const http = require("http"); // Import HTTP module
-const fs = require("fs");     // Import File System module
+const http = require('http');   // Import HTTP module
+const fs = require('fs');       // Import File System module
+const port = 3000;              // Port number
+
+// Function to handle incoming requests
+const handlereq = (req, res) => {
+    let filename = "";
+
+    // Decide which file to serve based on URL
+    switch (req.url) {
+        case '/':
+            filename = "Home.html";
+            break;
+        case '/About':
+            filename = "About.html";
+            break;
+        case '/Contact':
+            filename = "Contact.html";
+            break;
+        default:
+            filename = "404.html"; // Default page
+            break;
+    }
+
+    // Read and send the file
+    fs.readFile(filename, (err, result) => {
+        if (!err) {
+            res.end(result);
+        }
+    });
+};
 
 // Create server
-const server = http.createServer((req, res) => {
+const server = http.createServer(handlereq);
 
-  // Home route
-  if (req.url === "/") {
-    fs.readFile("Pages/home.html", (err, data) => {
-      res.write(data); // Send home page
-      res.end();
-    });
-
-  // About route
-  } else if (req.url === "/about") {
-    fs.readFile("Pages/about.html", (err, data) => {
-      res.write(data); // Send about page
-      res.end();
-    });
-
-  // Contact route
-  } else if (req.url === "/contact") {
-    fs.readFile("Pages/contact.html", (err, data) => {
-      res.write(data); // Send contact page
-      res.end();
-    });
-
-  // 404 route
-  } else {
-    res.write("<h1>404 Page Not Found</h1>"); // Page not found
-    res.end();
-  }
-});
-
-// Start server on port 3000
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+// Start server
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
